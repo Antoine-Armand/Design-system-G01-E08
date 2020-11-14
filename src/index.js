46ed88466b05b810619619d7a459d4396438e5b4
Antoine-Armand <antoine.armand99@gmail.com>
2020-11-14 18:59:19 +0100

    Initialisation components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-console.log('hello webpack');
-
 import './assets/styles/main.css'
-
-import image from './assets/images/image.jpg'
-const $img = new Image()
-$img.src = image
-document.body.appendChild($img)
-
-console.log('test');
