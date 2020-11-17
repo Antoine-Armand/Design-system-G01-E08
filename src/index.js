01f9a7b82a57495994b39ae31c42c4d1d7d416df
Antoine-Armand <antoine.armand99@gmail.com>
2020-11-17 12:03:03 +0100

    Update dark-mode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './assets/styles/main.scss'
+
+/*
+    Dark mode switcher
+*/
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+    console.log('hello');
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
